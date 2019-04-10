a882859da06dce131cede16507a8ed0c9878c9cd
hodevin <hod@allegheny.edu>
2019-04-10 10:45:03 -0400

    Changes to links

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -70,7 +70,7 @@ const NavigationPreview = ({ entry }) => {
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.PNG" alt="SPCNY"/>
+          <img src="/images/logo.PNG" alt="SCNY"/>
         </a>
       </div>
       <a className="nav-button">
